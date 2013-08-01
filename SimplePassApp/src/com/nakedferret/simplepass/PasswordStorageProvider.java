a81690c65f5a6a670cf25c5fbe3c2636c80f1433
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-01 10:54:22 -0400

    Added a skeleton for a content provider

---
+++ SimplePassApp/src/com/nakedferret/simplepass/PasswordStorageProvider.java
@@ -0,0 +1,70 @@
+package com.nakedferret.simplepass;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class PasswordStorageProvider extends ContentProvider {
+
+	private static final String provider = "com.nakedferret.simplepass.provider";
+
+	private static final int ACCOUNT = 1;
+	private static final int ACCOUNT_ID = 2;
+
+	private static final int VAULT = 3;
+	private static final int VAULT_ID = 4;
+
+	private static final UriMatcher sURIMatcher = new UriMatcher(
+			UriMatcher.NO_MATCH);
+
+	static {
+		sURIMatcher.addURI(provider, "account", ACCOUNT);
+		sURIMatcher.addURI(provider, "account/#", ACCOUNT_ID);
+		sURIMatcher.addURI(provider, "vault", VAULT);
+		sURIMatcher.addURI(provider, "vault/#", VAULT_ID);
+	}
+
+	public PasswordStorageProvider() {
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// Implement this to handle requests to delete one or more rows.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		// TODO: Implement this to handle requests for the MIME type of the data
+		// at the given URI.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO: Implement this to handle requests to insert a new row.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO: Implement this to initialize your content provider on startup.
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO: Implement this to handle query requests from clients.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO: Implement this to handle requests to update one or more rows.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+}
