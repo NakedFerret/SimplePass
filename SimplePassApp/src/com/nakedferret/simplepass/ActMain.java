2a91a87a4ecbd4b58e89382dc99b9ffa7f907e4a
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-07-31 23:11:42 -0400

    Added ABS as a dependency to the project

--- SimplePassApp/src/com/nakedferret/simplepass/ActMain.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ActMain.java
@@ -1,22 +1,15 @@
 package com.nakedferret.simplepass;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class ActMain extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class ActMain extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.act_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.act_main, menu);
-		return true;
 	}
 
 }
