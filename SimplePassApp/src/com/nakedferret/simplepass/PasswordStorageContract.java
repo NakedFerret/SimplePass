2010af31803cc8e1dab090cbfd72f42cea1678d2
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-01 00:43:25 -0400

    Changed the column name to avoid conflicts with sqlite3 keywords

--- SimplePassApp/src/com/nakedferret/simplepass/PasswordStorageContract.java
+++ SimplePassApp/src/com/nakedferret/simplepass/PasswordStorageContract.java
@@ -9,20 +9,20 @@ public final class PasswordStorageContract {
 
 	public static abstract class Account implements BaseColumns {
 		public static final String TABLE_NAME = "accounts";
-		public static final String COL_NAME = "name";
-		public static final String COL_GROUP = "group";
-		public static final String COL_USERNAME = "username";
-		public static final String COL_PASSWORD = "password";
-		public static final String COL_VAULT = "vault_id";
+		public static final String COL_NAME = "_name";
+		public static final String COL_GROUP = "_group";
+		public static final String COL_USERNAME = "_username";
+		public static final String COL_PASSWORD = "_password";
+		public static final String COL_VAULT = "_vault_id";
 	}
 
 	public static abstract class Vault implements BaseColumns {
 		public static final String TABLE_NAME = "vaults";
-		public static final String COL_NAME = "name";
-		public static final String COL_SALT = "salt";
-		public static final String COL_IV = "iv";
-		public static final String COL_ITERATIONS = "iterations";
-		public static final String COL_HASH = "hash";
+		public static final String COL_NAME = "_name";
+		public static final String COL_SALT = "_salt";
+		public static final String COL_IV = "_iv";
+		public static final String COL_ITERATIONS = "_iterations";
+		public static final String COL_HASH = "_hash";
 	}
 
 }
