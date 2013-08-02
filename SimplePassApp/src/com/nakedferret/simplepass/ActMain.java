b5fde6648bff77f2911c554c20b0063632580008
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-02 10:41:08 -0400

    Added insert button
    to test the content provider in the main activity

--- SimplePassApp/src/com/nakedferret/simplepass/ActMain.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ActMain.java
@@ -1,11 +1,17 @@
 package com.nakedferret.simplepass;
 
+import android.widget.Button;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.act_main)
 public class ActMain extends SherlockActivity {
+
+	@ViewById
+	Button insertButton;
 
 	@AfterViews
 	void initDatabase() {
