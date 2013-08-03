528435fdbb8f4e6fe1ce7aa0364b419559896c2e
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-03 10:42:48 -0400

    Added an activity to show all the accounts

---
+++ SimplePassApp/src/com/nakedferret/simplepass/ActSelectAccount.java
@@ -0,0 +1,12 @@
+package com.nakedferret.simplepass;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.act_select_account)
+public class ActSelectAccount extends Activity {
+	
+	
+
+}
