504775d86c8ec54458b1145292bed1538dcd5723
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-03 11:06:34 -0400

    Added account selection implements the support loader

--- SimplePassApp/src/com/nakedferret/simplepass/ActSelectAccount.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ActSelectAccount.java
@@ -1,12 +1,40 @@
 package com.nakedferret.simplepass;
 
-import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
 
-@EActivity(R.layout.act_select_account)
-public class ActSelectAccount extends Activity {
-	
-	
+@EActivity
+public class ActSelectAccount extends SherlockFragmentActivity implements
+		LoaderCallbacks<Cursor> {
+
+	@AfterViews
+	void initInterface() {
+
+	}
+
+	@Override
+	public android.support.v4.content.Loader<Cursor> onCreateLoader(int arg0,
+			Bundle arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(android.support.v4.content.Loader<Cursor> arg0,
+			Cursor arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onLoaderReset(android.support.v4.content.Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
