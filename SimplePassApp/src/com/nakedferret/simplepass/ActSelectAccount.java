ad34463f4b760550dabc264743d2c46bed3fbd31
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-05 23:59:37 -0400

    Created a floating activity
    However, it does not dismiss when the user touches outside

--- SimplePassApp/src/com/nakedferret/simplepass/ActSelectAccount.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ActSelectAccount.java
@@ -1,8 +1,13 @@
 package com.nakedferret.simplepass;
 
 import android.database.Cursor;
+import android.hardware.display.DisplayManager;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.googlecode.androidannotations.annotations.AfterViews;
@@ -16,6 +21,29 @@ public class ActSelectAccount extends SherlockFragmentActivity implements
 
 	@FragmentById
 	FragListAccounts accountFragment;
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		showAsPopup();
+	}
+
+	private void showAsPopup() {
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+				WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+		WindowManager.LayoutParams params = getWindow().getAttributes();
+		params.alpha = 1.0f;
+		params.dimAmount = 0.5f;
+		getWindow().setAttributes(params);
+
+		// This sets the window size, while working around the
+		// IllegalStateException thrown by ActionBarView
+		Display dm = getWindowManager().getDefaultDisplay();
+		int width = (int) (dm.getWidth() * .9);
+		int height = (int) (dm.getHeight() * .9);
+		getWindow().setLayout(width, height);
+	}
 
 	@AfterViews
 	void initInterface() {
