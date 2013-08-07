a32d7a23fc81e187ec043a4c13b23a9ee51418e2
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-07 00:55:25 -0400

    Removed extra imports

--- SimplePassApp/src/com/nakedferret/simplepass/FragListCursor.java
+++ SimplePassApp/src/com/nakedferret/simplepass/FragListCursor.java
@@ -3,10 +3,6 @@ package com.nakedferret.simplepass;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
