3773c31396faaa350da4ccb620761a7f98874a09
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-07 01:26:59 -0400

    Added a new Act to test the new Generic Frag

---
+++ SimplePassApp/src/com/nakedferret/simplepass/ActGenericFragTest.java
@@ -0,0 +1,48 @@
+package com.nakedferret.simplepass;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.nakedferret.simplepass.PasswordStorageContract.Account;
+import com.nakedferret.simplepass.PasswordStorageContract.Vault;
+
+@EActivity(R.layout.act_generic_frag_test)
+public class ActGenericFragTest extends ActFloating {
+
+	@Click(R.id.vaultButton)
+	void showVaultsFragment() {
+		int layout = android.R.layout.simple_list_item_1;
+		Uri uri = Utils.buildContentUri(Vault.TABLE_NAME);
+		String[] projection = { Vault.COL_NAME };
+		int[] views = { android.R.id.text1 };
+		String searchCol = Vault.COL_NAME;
+
+		FragListCursor vaultsList = FragListCursor.newInstance(layout, uri,
+				projection, views, searchCol);
+
+		FragmentManager m = getSupportFragmentManager();
+		FragmentTransaction t = m.beginTransaction();
+		t.replace(R.id.listFragment, vaultsList);
+		t.commit();
+	}
+
+	@Click(R.id.accountsButton)
+	void showAccountsFragment() {
+		int layout = android.R.layout.simple_list_item_1;
+		Uri uri = Utils.buildContentUri(Account.TABLE_NAME);
+		String[] projection = { Account.COL_NAME };
+		int[] views = { android.R.id.text1 };
+		String searchCol = Account.COL_NAME;
+
+		FragListCursor vaultsList = FragListCursor.newInstance(layout, uri,
+				projection, views, searchCol);
+
+		FragmentManager m = getSupportFragmentManager();
+		FragmentTransaction t = m.beginTransaction();
+		t.replace(R.id.listFragment, vaultsList);
+		t.commit();
+	}
+}
