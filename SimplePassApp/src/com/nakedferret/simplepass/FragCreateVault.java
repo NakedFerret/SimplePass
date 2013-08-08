7baed2d0ec61e09d704fca0351011d33dbe786c2
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-08 17:00:03 -0400

    Created the FragCreateVault interface

--- SimplePassApp/src/com/nakedferret/simplepass/FragCreateVault.java
+++ SimplePassApp/src/com/nakedferret/simplepass/FragCreateVault.java
@@ -2,23 +2,35 @@ package com.nakedferret.simplepass;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EFragment;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.res.StringArrayRes;
 
 @EFragment(R.layout.frag_create_vault)
-public class FragCreateVault extends SherlockFragment {
+public class FragCreateVault extends SherlockFragment implements
+		OnItemSelectedListener {
+
+	@ViewById
+	Spinner secondsSpinner;
+
+	@StringArrayRes
+	String[] secondsArray;
+
+	private ArrayAdapter<String> adapter;
 
 	private OnVaultCreatedListener mListener;
 
 	public FragCreateVault() {
 		// Required empty public constructor
-	}
-
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onVaultSelected(uri);
-		}
 	}
 
 	@Override
@@ -40,6 +52,26 @@ public class FragCreateVault extends SherlockFragment {
 
 	public interface OnVaultCreatedListener {
 		public void onVaultSelected(Uri uri);
+	}
+
+	@AfterViews
+	void initSpinner() {
+		adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_spinner_dropdown_item, secondsArray);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		secondsSpinner.setAdapter(adapter);
+		secondsSpinner.setOnItemSelectedListener(this);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View v, int position,
+			long id) {
+		Log.d("SimplePass", "Spinner Selected...pos,id: " + position + "," + id);
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+
 	}
 
 }
