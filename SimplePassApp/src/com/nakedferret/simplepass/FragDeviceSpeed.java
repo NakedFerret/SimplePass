19d7805e0ec861f9f2c27af1bec6569ad157a515
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-08 20:16:16 -0400

    Created new fragment to determine the amount of aes iterations the device can do in one second

---
+++ SimplePassApp/src/com/nakedferret/simplepass/FragDeviceSpeed.java
@@ -0,0 +1,38 @@
+package com.nakedferret.simplepass;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+
+import com.googlecode.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.frag_device_speed)
+public class FragDeviceSpeed extends Fragment {
+
+	private OnIterationsCalculatedListener mListener;
+
+	public FragDeviceSpeed() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			mListener = (OnIterationsCalculatedListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement OnFragmentInteractionListener");
+		}
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mListener = null;
+	}
+
+	public interface OnIterationsCalculatedListener {
+		public void onIterationsCalculated(int iters);
+	}
+
+}
