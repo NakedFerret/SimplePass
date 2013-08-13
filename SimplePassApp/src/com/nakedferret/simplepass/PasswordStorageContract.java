5de48932f241541945c8d9d7d416338f0ac304ad
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-13 10:11:04 -0400

    Added group table to the contract

--- SimplePassApp/src/com/nakedferret/simplepass/PasswordStorageContract.java
+++ SimplePassApp/src/com/nakedferret/simplepass/PasswordStorageContract.java
@@ -8,21 +8,27 @@ public final class PasswordStorageContract {
 	}
 
 	public static abstract class Account implements BaseColumns {
-		public static final String TABLE_NAME = "accounts";
-		public static final String COL_NAME = "_name";
-		public static final String COL_GROUP = "_group";
-		public static final String COL_USERNAME = "_username";
-		public static final String COL_PASSWORD = "_password";
-		public static final String COL_VAULT = "_vault_id";
+		public static final String TABLE_NAME = "account";
+		public static final String COL_NAME = "name";
+		public static final String COL_GROUP = "group_id";
+		public static final String COL_USERNAME = "username";
+		public static final String COL_PASSWORD = "password";
+		public static final String COL_VAULT = "vault_id";
 	}
 
 	public static abstract class Vault implements BaseColumns {
-		public static final String TABLE_NAME = "vaults";
-		public static final String COL_NAME = "_name";
-		public static final String COL_SALT = "_salt";
-		public static final String COL_IV = "_iv";
-		public static final String COL_ITERATIONS = "_iterations";
-		public static final String COL_HASH = "_hash";
+		public static final String TABLE_NAME = "vault";
+		public static final String COL_NAME = "name";
+		public static final String COL_SALT = "salt";
+		public static final String COL_IV = "iv";
+		public static final String COL_ITERATIONS = "iterations";
+		public static final String COL_HASH = "hash";
+	}
+
+	public static abstract class Group implements BaseColumns {
+		// Group is a special word in SQL
+		public static final String TABLE_NAME = "category";
+		public static final String COL_NAME = "name";
 	}
 
 }
