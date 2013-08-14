2c31b61addacc65ce4e3f93eaf9a748500f73d4e
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-08-14 00:40:59 -0400

    Updated the test data button methods

--- SimplePassApp/src/com/nakedferret/simplepass/ActMain.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ActMain.java
@@ -3,7 +3,7 @@ package com.nakedferret.simplepass;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Button;
@@ -15,6 +15,7 @@ import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.ViewById;
 import com.nakedferret.simplepass.PasswordStorageContract.Account;
+import com.nakedferret.simplepass.PasswordStorageContract.Group;
 import com.nakedferret.simplepass.PasswordStorageContract.Vault;
 
 @EActivity(R.layout.act_main)
@@ -54,21 +55,6 @@ public class ActMain extends SherlockActivity {
 		new PasswordStorageDbHelper(this).getWritableDatabase().close();
 	}
 
-	@Click(R.id.insertButton)
-	void onInsert() {
-		doBackgroundInsert();
-	}
-
-	@Background
-	void doBackgroundInsert() {
-		ContentResolver r = getContentResolver();
-
-		ContentValues values = new ContentValues();
-		values.put(Vault.COL_NAME, "testVault");
-
-		r.insert(Utils.buildContentUri(Vault.TABLE_NAME), values);
-	}
-
 	@Click(R.id.testDataButton)
 	void onCreateTestData() {
 		clearAndInsertTestData();
@@ -77,56 +63,42 @@ public class ActMain extends SherlockActivity {
 	@Background
 	void clearAndInsertTestData() {
 		PasswordStorageDbHelper helper = new PasswordStorageDbHelper(this);
-		SQLiteDatabase db = helper.getWritableDatabase();
-		ContentValues values = new ContentValues();
 		helper.clearData();
-
-		values.put(Vault.COL_ITERATIONS, 5000);
-		values.put(Vault.COL_NAME, "Personal");
-		long vaultId = db.insert(Vault.TABLE_NAME, null, values);
-		values.clear();
-
-		values.put(Account.COL_NAME, "reddit");
-		values.put(Account.COL_VAULT, vaultId);
-		values.put(Account.COL_GROUP, "entertainment");
-		values.put(Account.COL_USERNAME, "naked_ferret");
-		values.put(Account.COL_PASSWORD, "secret");
-		db.insert(Account.TABLE_NAME, null, values);
-		values.clear();
-
-		values.put(Account.COL_NAME, "xda-developers");
-		values.put(Account.COL_VAULT, vaultId);
-		values.put(Account.COL_GROUP, "development");
-		values.put(Account.COL_USERNAME, "naked_ferret");
-		values.put(Account.COL_PASSWORD, "secret2");
-		db.insert(Account.TABLE_NAME, null, values);
-		values.clear();
-
-		values.put(Account.COL_NAME, "twitter");
-		values.put(Account.COL_VAULT, vaultId);
-		values.put(Account.COL_GROUP, "social");
-		values.put(Account.COL_USERNAME, "naked_ferret");
-		values.put(Account.COL_PASSWORD, "secret3");
-		db.insert(Account.TABLE_NAME, null, values);
-		values.clear();
-
-		values.put(Account.COL_NAME, "bank");
-		values.put(Account.COL_VAULT, vaultId);
-		values.put(Account.COL_GROUP, "financial");
-		values.put(Account.COL_USERNAME, "naked_ferret");
-		values.put(Account.COL_PASSWORD, "secret4");
-		db.insert(Account.TABLE_NAME, null, values);
-		values.clear();
-
-		values.put(Account.COL_NAME, "amazon");
-		values.put(Account.COL_VAULT, vaultId);
-		values.put(Account.COL_GROUP, "retail");
-		values.put(Account.COL_USERNAME, "naked_ferret");
-		values.put(Account.COL_PASSWORD, "secret5");
-		db.insert(Account.TABLE_NAME, null, values);
-		values.clear();
-
 		helper.close();
+
+		ContentResolver r = getContentResolver();
+
+		ContentValues vault = Utils.createVault("Personal", "secret", 2500);
+		Uri rowUri = r.insert(Utils.buildContentUri(Vault.TABLE_NAME), vault);
+		int vaultId = Integer.parseInt(rowUri.getLastPathSegment());
+
+		insertAccount("Reddit", "Entertainment", vaultId);
+		insertAccount("Xda-Developers", "Development", vaultId);
+		insertAccount("Twitter", "Social", vaultId);
+		insertAccount("Bank", "Financial", vaultId);
+		insertAccount("Amazon", "Retail", vaultId);
+	}
+
+	private void insertAccount(String name, String group, int vaultId) {
+		ContentResolver r = getContentResolver();
+		ContentValues values = new ContentValues();
+
+		values.put(Group.COL_NAME, group);
+		Uri rowUri = r.insert(Utils.buildContentUri(Group.TABLE_NAME), values);
+
+		values.clear();
+
+		final String username = "naked_ferret";
+		final String pass = "super_secret";
+		final int groupID = Integer.parseInt(rowUri.getLastPathSegment());
+
+		values.put(Account.COL_GROUP, groupID);
+		values.put(Account.COL_NAME, name);
+		values.put(Account.COL_PASSWORD, pass);
+		values.put(Account.COL_USERNAME, username);
+		values.put(Account.COL_VAULT, vaultId);
+
+		r.insert(Utils.buildContentUri(Account.TABLE_NAME), values);
 	}
 
 	@Click(R.id.testQuery)
