cfa515180786030e7a8035e720f19cef23f7add8
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-09-01 11:50:28 -0400

    Added some logic to unlock a vault when an intent is received

--- SimplePassApp/src/com/nakedferret/simplepass/ServicePassword.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ServicePassword.java
@@ -1,5 +1,14 @@
 package com.nakedferret.simplepass;
 
+import java.security.Key;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.spongycastle.util.encoders.Hex;
+
 import android.app.Service;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -50,6 +59,7 @@ public class ServicePassword extends Service {
 		}
 	}
 
+	// Use this method to join back to the UI thread
 	@UiThread
 	void dispatchIntent(Intent i) {
 
@@ -58,7 +68,31 @@ public class ServicePassword extends Service {
 	@Background
 	void unlockVault(Uri uri, String pass) {
 		Utils.log(this, "Going to attempt to unlock vault: " + uri.toString());
+		Utils.log(this, "with the password: " + pass);
+
 		ContentValues vault = getVault(uri);
+		Utils.log(this, "Got the vault: " + vault.getAsString(Vault.COL_NAME));
+		byte[] iv = Hex.decode(vault.getAsString(Vault.COL_IV));
+		byte[] salt = Hex.decode(vault.getAsString(Vault.COL_SALT));
+
+		byte[] storedHash = Hex.decode(vault.getAsString(Vault.COL_HASH));
+		int iterations = vault.getAsInteger(Vault.COL_ITERATIONS);
+		byte[] keyValue = Utils.getKey(pass, salt, iterations);
+
+		try {
+			Key key = new SecretKeySpec(keyValue, Utils.KEY_SPEC);
+			Cipher c = Cipher.getInstance(Utils.ENCRYPTION_CIPHER);
+
+			c.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+			byte[] encPass = c.doFinal(pass.getBytes("UTF-8"));
+			byte[] hash = Utils.getHash(encPass, salt);
+
+			Utils.log(this,
+					"hash are the same: " + Arrays.equals(hash, storedHash));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		// TODO: implement unlock vault
 		// generate the key, check the hash,
 		// and send an intent that the vault was unlocked if successful and
@@ -74,7 +108,10 @@ public class ServicePassword extends Service {
 	private ContentValues getVault(Uri uri) {
 		ContentValues vault = new ContentValues();
 
+		Utils.log(this, uri.toString());
+
 		Cursor c = getContentResolver().query(uri, null, null, null, null);
+		// TODO: implement getting the exact vault in query
 		c.moveToFirst();
 
 		vault.put(Vault.COL_HASH, c.getString(c.getColumnIndex(Vault.COL_HASH)));
@@ -83,6 +120,7 @@ public class ServicePassword extends Service {
 		vault.put(Vault.COL_IV, c.getString(c.getColumnIndex(Vault.COL_IV)));
 		vault.put(Vault.COL_NAME, c.getString(c.getColumnIndex(Vault.COL_NAME)));
 		vault.put(Vault.COL_SALT, c.getString(c.getColumnIndex(Vault.COL_SALT)));
+		c.close();
 		return vault;
 	}
 }
