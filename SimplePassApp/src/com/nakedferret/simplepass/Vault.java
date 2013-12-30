6596f907bbba04e73d3e1225f7ceea3d69af4c1d
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-12-30 17:56:04 -0500

    Added Vault.createVault()

--- SimplePassApp/src/com/nakedferret/simplepass/Vault.java
+++ SimplePassApp/src/com/nakedferret/simplepass/Vault.java
@@ -1,5 +1,11 @@
 package com.nakedferret.simplepass;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
@@ -21,11 +27,11 @@ public class Vault extends Model {
 
 	@Column
 	public byte[] hash;
-	
+
 	public Vault() {
-		
+
 	}
-	
+
 	public Vault(String name, byte[] salt, byte[] iv, int iterations,
 			byte[] hash) {
 		this.name = name;
@@ -33,6 +39,21 @@ public class Vault extends Model {
 		this.iv = iv;
 		this.iterations = iterations;
 		this.hash = hash;
+	}
+
+	public static Vault createVault(String name, String password, int iterations) {
+		Vault v = new Vault();
+		v.name = name;
+		v.iterations = iterations;
+		v.salt = Utils.getSalt();
+
+		byte[] key = Utils.getKey(password, v.salt, iterations);
+		Cipher cipher = Utils.getEncryptionCipher(key);
+		byte[] encPass = Utils.encrypt(cipher, password);
+
+		v.iv = cipher.getIV();
+		v.hash = Utils.getHash(encPass, v.salt);
+		return v;
 	}
 
 }
