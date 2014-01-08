abe89300814b5f7195494e942d8dd0fe6066aecb
Gonzalo Andreani <gonzaandreani@gmail.com>
2014-01-08 18:45:23 -0500

    Added ActTutorial

---
+++ SimplePassApp/src/com/nakedferret/simplepass/ui/ActTutorial.java
@@ -0,0 +1,12 @@
+package com.nakedferret.simplepass.ui;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.nakedferret.simplepass.R;
+
+@EActivity(R.layout.act_tutorial)
+public class ActTutorial extends Activity {
+
+	
+}
