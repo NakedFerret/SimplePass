01c73a9c0780b89bd3ac82dbbfda5e77e09b03a9
Gonzalo Andreani <gonzaandreani@gmail.com>
2014-01-14 13:48:37 -0500

    Added a menu item that launches a file picker

--- SimplePassApp/src/com/nakedferret/simplepass/ui/ActMain.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ui/ActMain.java
@@ -1,6 +1,9 @@
 package com.nakedferret.simplepass.ui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
@@ -37,6 +40,30 @@ public class ActMain extends Activity {
 			overlayButton.setText(R.string.stop_overlay);
 		}
 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.act_main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_import:
+			launchFilePicker();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void launchFilePicker() {
+		Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+		i.addCategory(Intent.CATEGORY_OPENABLE);
+		i.setType("file/*");
+		startActivity(i);
 	}
 
 	@Override
