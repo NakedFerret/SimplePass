6eaf2b15fb0de3ec11e08058edb3b9b41ca4cf9b
Gonzalo Andreani <gonzaandreani@gmail.com>
2014-01-28 23:22:14 -0500

    FragImportEditAccount selects the similar category in the categorySpinner

--- SimplePassApp/src/com/nakedferret/simplepass/ui/FragImportEditAccount.java
+++ SimplePassApp/src/com/nakedferret/simplepass/ui/FragImportEditAccount.java
@@ -3,19 +3,33 @@ package com.nakedferret.simplepass.ui;
 import java.util.List;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.ViewSwitcher;
 
+import com.activeandroid.content.ContentProvider;
 import com.activeandroid.query.Select;
 import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.Bean;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EFragment;
 import com.googlecode.androidannotations.annotations.FragmentArg;
 import com.googlecode.androidannotations.annotations.OptionsItem;
 import com.googlecode.androidannotations.annotations.OptionsMenu;
+import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
 import com.nakedferret.simplepass.Category;
 import com.nakedferret.simplepass.ImportManager;
@@ -25,7 +39,8 @@ import com.nakedferret.simplepass.utils.Utils;
 
 @EFragment(R.layout.frag_import_edit_account)
 @OptionsMenu(R.menu.ac_frag_import_edit_account)
-public class FragImportEditAccount extends Fragment {
+public class FragImportEditAccount extends Fragment implements
+		LoaderCallbacks<Cursor> {
 
 	@FragmentArg
 	long selectedAccount;
@@ -42,7 +57,9 @@ public class FragImportEditAccount extends Fragment {
 	@ViewById
 	ViewSwitcher categoryEditSwitcher;
 
-	ActImport activity;
+	private ActImport activity;
+	private SimpleCursorAdapter categoryAdapter;
+	private Long similarCategoryID;
 
 	public FragImportEditAccount() {
 		// Required empty public constructor
@@ -56,11 +73,19 @@ public class FragImportEditAccount extends Fragment {
 
 	@AfterViews
 	void init() {
-		ImportAccount a = importManager.getSelectedAccount(selectedAccount);
-		Utils.log(this, "account category: " + a.getCategory());
-		List<Category> list = new Select().from(Category.class)
-				.where("name = ? collate nocase", a.getCategory()).execute();
-		Utils.log(this, "similar groups: " + list.size());
+		prepareCategorySpinner();
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	private void prepareCategorySpinner() {
+		String[] from = { "name" };
+		int[] to = { android.R.id.text1 };
+
+		categoryAdapter = new SimpleCursorAdapter(activity,
+				android.R.layout.simple_spinner_item, null, from, to, 0);
+		categoryAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		categorySpinner.setAdapter(categoryAdapter);
 	}
 
 	@Click(R.id.editButton)
@@ -81,6 +106,50 @@ public class FragImportEditAccount extends Fragment {
 	@OptionsItem(R.id.action_discard)
 	void discardChanges() {
 		activity.finishAccountEdit();
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		Uri categoryUri = ContentProvider.createUri(Category.class, null);
+		return new CursorLoader(activity, categoryUri, null, null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		categoryAdapter.changeCursor(data);
+		findSimilarCategory();
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		categoryAdapter.changeCursor(null);
+	}
+
+	@Background
+	void findSimilarCategory() {
+		ImportAccount a = importManager.getSelectedAccount(selectedAccount);
+		Category cat = new Select().from(Category.class)
+				.where("name = ? collate nocase", a.getCategory())
+				.executeSingle();
+		Utils.log(this, "category: " + cat);
+
+		if (cat == null)
+			return;
+
+		similarCategoryID = cat.getId();
+
+		for (int i = 0; i < categorySpinner.getCount(); i++) {
+			if (categorySpinner.getItemIdAtPosition(i) == similarCategoryID) {
+				Utils.log(this, "findSimilarCategory");
+				setSpinnerSelection(i);
+				break;
+			}
+		}
+	}
+
+	@UiThread
+	void setSpinnerSelection(int categoryPosition) {
+		categorySpinner.setSelection(categoryPosition);
 	}
 
 }
